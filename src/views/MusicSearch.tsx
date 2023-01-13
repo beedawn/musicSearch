1030b518a26c398b2af48f8f98e3c37997b1cbc0
bee <brandonschmersal@gmail.com>
2023-01-12 22:28:38 -0500

    third

--- src/views/MusicSearch.tsx
+++ src/views/MusicSearch.tsx
@@ -1,47 +1,9 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
 
-import Options from './secrets/Options';
+import Options from '../secrets/Options';
 
-interface musicData {
-  album: {
-    cover: string,
-    cover_big:string,
-    cover_small:string,
-    cover_xl:string,
-    id:number,
-    md5_image:string,
-    title:string,
-    tracklist:string,
-    type:string
-  },
-  artist:{
-    id:number,
-    link:string,
-    name:string,
-    picture:string,
-    picture_big:string,
-    picture_medium:string,
-    picture_small:string,
-    picture_xl:string,
-    tracklist:string,
-    type:string
-  },
-  duration:number,
-  explicit_content_cover:number,
-  explicit_content_lyrics:number,
-  explicit_lyrics:boolean,
-  id:number,
-  link:string,
-  md5_image:string,
-  preview:string,
-  rank:number,
-  title:string,
-  title_short:string,
-  title_version:string,
-  type:string
-
-}
+import musicData from '../interfaces/MusicData';
 
 
 
