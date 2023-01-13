7e4a91e8e6f0ccb439c8a41eecd4ecc7ff09ac36
bee <brandonschmersal@gmail.com>
2023-01-13 07:45:47 -0500

    six

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Results from './components/Results';
 import SearchBar from './components/SearchBar';
 
+import Options from "../secrets/Options";
+
 function Main() {
 
+  
     const[userInput,setUserInput]=useState("");
-let query:string="";
+    function setHandleData(data:any){
+console.log(data);
+console.log("hello");
+    }
+    function callAPI(){
+        const options = Options;
+        const url:string="https://deezerdevs-deezer.p.rapidapi.com/search?q="+userInput;
+  
+        fetch(url, options)
+        .then((response) => response.json())
+        .then((response) => setHandleData(response.data))
+        .catch((err) => console.error(err));
+    }
+
+
+
     function handleSubmit(event: any, userInput:any) {
-        query=userInput;
+      callAPI();
         console.log("pizza");
         console.log(userInput);
         event.preventDefault();
@@ -19,7 +37,7 @@ let query:string="";
     <div className="App">
         <SearchBar userInput={userInput} setUserInput={setUserInput} handleSubmit={handleSubmit}/>
         yolo
-     <Results query={query}/>
+     <Results query={userInput}/>
      </div>
   );
 }
--- src/views/components/Results.tsx
+++ src/views/components/Results.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Options from "../../secrets/Options";
 import MusicData from "../../interfaces/MusicData";
 
 function Results(props:{
@@ -10,16 +9,7 @@ function Results(props:{
   const [handleData, setHandleData] = useState<MusicData | undefined>(
     undefined
   );
-const query=props.query;
-  useEffect(() => {
-    const options = Options;
-    const url:string="https://deezerdevs-deezer.p.rapidapi.com/search?q="+query;
-console.log(query);
-    fetch(url, options)
-      .then((response) => response.json())
-      .then((response) => setHandleData(response.data))
-      .catch((err) => console.error(err));
-  }, [query]);
+
   console.log("uyoloy");
   console.log(handleData);
   while (!handleData) {
