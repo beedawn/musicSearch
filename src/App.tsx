888ce808a374f65816a12b56cd6a3f3fc123e2a3
bee <brandonschmersal@gmail.com>
2023-01-12 22:14:55 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MusicSearch from './views/MusicSearch';
+
+function App() {
+  return (
+    <div className="App">
+     <MusicSearch />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/views/MusicSearch.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+
+import Options from '../secrets/Options';
+
+interface musicData {
+  album: {
+    cover: string,
+    cover_big:string,
+    cover_small:string,
+    cover_xl:string,
+    id:number,
+    md5_image:string,
+    title:string,
+    tracklist:string,
+    type:string
+  },
+  artist:{
+    id:number,
+    link:string,
+    name:string,
+    picture:string,
+    picture_big:string,
+    picture_medium:string,
+    picture_small:string,
+    picture_xl:string,
+    tracklist:string,
+    type:string
+  },
+  duration:number,
+  explicit_content_cover:number,
+  explicit_content_lyrics:number,
+  explicit_lyrics:boolean,
+  id:number,
+  link:string,
+  md5_image:string,
+  preview:string,
+  rank:number,
+  title:string,
+  title_short:string,
+  title_version:string,
+  type:string
+
+}
+
+
+
+
+
+
+function MusicSearch() {
+
+  useEffect(()=>{const options = Options;
+  
+  fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem', options)
+    .then(response => response.json())
+    .then(response => handleData(response.data))
+    .catch(err => console.error(err));},[]);
+
+
+    return (
+      <div>
+        Search Land
+      </div>
+    );
+  }
+
+
+  function handleData(data:musicData){
+    console.log(data);
+  }
+  
+  export default MusicSearch;
+  
