4970b87f937917590b9258a421704dc967e50142
bee <brandonschmersal@gmail.com>
2023-01-13 23:24:23 -0500

    fourteen

--- src/views/ThemeHandler.tsx
+++ src/views/ThemeHandler.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import {useState} from 'react';
-import { MDBSwitch } from 'mdb-react-ui-kit';
-import Main from './Main';
+import React from "react";
+import { useState, useEffect } from "react";
+import { MDBSwitch } from "mdb-react-ui-kit";
+import Main from "./Main";
 import '../styles/styles.css';
 function ThemeHandler() {
+  const [darkModeTheme, setDarkModeTheme]=useState(false);
+
+  if(darkModeTheme){
+return(
+  
+  <div><MDBSwitch id='darkMode' className="switch" label='darkmode'/>
+  <Main /></div>
+)
+  }else{
   return (
-    <div className="App">
-      {/* <MDBSwitch id='darkMode' className="switch" label='darkmode'/> */}
-     <Main />
-     </div>
-  );
+    <div className="darkTheme">
+     
+      <MDBSwitch id='darkMode' className="switch" label='darkmode'/>
+     <div className="darkTheme"><Main/>pizza</div>
+    </div>
+  );}
 }
 
 export default ThemeHandler;
--- src/views/components/SingleResult.tsx
+++ src/views/components/SingleResult.tsx
@@ -1,62 +1,49 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import MusicData from "../../interfaces/MusicData";
-import {Col, Row, Card,CardBody,CardTitle,CardSubtitle,CardText,CardLink } from "reactstrap"; 
+import {
+  Col,
+  Row,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  CardLink,
+} from "reactstrap";
 import ReactAudioPlayer from "react-audio-player";
-function SingleResult(props:{
-song:any
-
-}) {
-const song = props.song;
- return(
-
-
-
-    
-      <Col>  
-        <Card
-  style={{
-    width:"100%",
-    marginTop:"5%",
-    alignItems:"center"
-  }}
->
-  <CardBody>
-    <CardTitle tag="h5">
-    {song.title}
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-     {song.artist.name}
-    </CardSubtitle>
-  </CardBody>
-  <img
-    alt="Card cap"
-    src={song.album.cover_xl}
-    width="100%"
-  />
-  <CardBody>
-    <CardText>
-    <ReactAudioPlayer
-  src={song.preview}
-  controls
-  style={{width:"100%"}}
-/>
-        </CardText>
-    <CardLink href={song.artist.link}>
-      Link to Artist
-    </CardLink>
-    <CardLink href={song.link}>
-      Song Info
-    </CardLink>
-  </CardBody>
-</Card>
-</Col>
-        
-        
-  )}
-
+function SingleResult(props: { song: any }) {
+  const song = props.song;
+  return (
+    <Col>
+      <Card
+        style={{
+          width: "100%",
+          marginTop: "5%",
+          alignItems: "center",
+        }}
+      >
+        <CardBody>
+          <CardTitle tag="h5">{song.title}</CardTitle>
+          <CardSubtitle className="mb-2 text-muted" tag="h6">
+            {song.artist.name}
+          </CardSubtitle>
+        </CardBody>
+        <img alt="Card cap" src={song.album.cover_xl} width="100%" />
+        <CardBody>
+          <CardText>
+            <ReactAudioPlayer
+              src={song.preview}
+              controls
+              style={{ width: "100%" }}
+            />
+          </CardText>
+          <CardLink href={song.artist.link}>Link to Artist</CardLink>
+          <CardLink href={song.link}>Song Info</CardLink>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
 
 export default SingleResult;
